4d2e8a2d8f6544a19fe3aeb4cb2e64f392c6e260
Mark Polak <mark@thedutchies.com>
2017-04-01 21:19:45 +0200

    First script :)

---
+++ Chapter2/FirstScript.hs
@@ -0,0 +1,31 @@
+{-
+
+-}
+
+module FirstScript where
+
+-- The value size is an integer (Integer), defined to be
+-- the sum of twelve and thirteen.
+
+size :: Integer
+size = 12 + 13
+
+-- The function to square an integer.
+
+square :: Integer -> Integer
+square n = n * n
+
+-- The function to double an integer.
+
+double :: Integer -> Integer
+double n = 2 * n
+
+-- An example using double, square and size.
+
+example :: Integer
+example = double (size - square ( 2 + 2))
+
+--  4 = 2 + 2
+-- 16 = 4 * 4 
+--  9 = 25 - 16
+-- 18 = 2 * 9
